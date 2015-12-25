9b67f0a409756e1295d6ecfd8917134fa7d762dc
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2015-12-25 11:09:12 +0200

    dummy data in list view

--- app/src/main/java/com/luxtech_eg/movieapp/MoviesListFragment.java
+++ app/src/main/java/com/luxtech_eg/movieapp/MoviesListFragment.java
@@ -1,6 +1,7 @@
 package com.luxtech_eg.movieapp;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +17,8 @@ import java.util.ArrayList;
 public class MoviesListFragment extends Fragment {
     ListView moviesLV;
     ArrayList<String> moviesAL = new ArrayList<String>();
-
+    ArrayAdapter <String> moviesAdapter;
+    final static String APIKEY= "52665819dfedc14d67605e2bb09ed729";
     public MoviesListFragment(){
 
     }
@@ -30,8 +32,43 @@ public class MoviesListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.movies_list_fragment_layout,container,false);
         moviesLV= (ListView) rootView.findViewById(R.id.listview_movies);
-        ArrayAdapter <String> moviesAdapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_movie,R.id.tv_movie_name,moviesAL);
+        moviesAdapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_movie,R.id.tv_movie_name,moviesAL);
         moviesLV.setAdapter(moviesAdapter);
+        //TODO remove the following lines later start
+        populateDummyData();
+        moviesAdapter.notifyDataSetChanged();
+        //// TODO: end
         return rootView;
     }
+    void populateDummyData(){
+        moviesAL.add("hha1");
+        moviesAL.add("hha2");
+        moviesAL.add("hha3");
+        moviesAL.add("hha4");
+    }
+    String buildPopularMoviesUri(){
+        //http://api.themoviedb.org/3/discover/movie?api_key=&sort_by=popularity.desc
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http").authority("api.themoviedb.org")
+                .appendPath("3")
+                .appendPath("discover")
+                .appendPath("movie")
+                .appendQueryParameter("api_key", APIKEY )
+                .appendQueryParameter("sort_by", "popularity.desc");
+        return builder.build().toString();
+    }
+
+    String buildTopRatedMoviesUri(){
+        //http://api.themoviedb.org/3/discover/movie?api_key=52665819dfedc14d67605e2bb09ed729&sort_by=vote_average.desc
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http").authority("api.themoviedb.org")
+                .appendPath("3")
+                .appendPath("discover")
+                .appendPath("movie")
+                .appendQueryParameter("api_key", APIKEY )
+                .appendQueryParameter("sort_by", "vote_average.desc");
+
+        return builder.build().toString();
+    }
+
 }
