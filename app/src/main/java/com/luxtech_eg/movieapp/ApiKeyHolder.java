0c1d86d230a2630a9e9f8bf51493a80f986c7d5e
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2015-12-30 14:41:46 +0200

    api key holder

---
+++ app/src/main/java/com/luxtech_eg/movieapp/ApiKeyHolder.java
@@ -0,0 +1,12 @@
+package com.luxtech_eg.movieapp;
+
+/**
+ * Created by ahmed on 30/12/15.
+ */
+public class ApiKeyHolder {
+    final static String APIKEY= "[api key]";
+
+    public static String getAPIKEY() {
+        return APIKEY;
+    }
+}
